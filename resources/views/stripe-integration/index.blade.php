1be13941cf058a86a45874674a1b0df0cf044581
jahanzaib sarri <jahanzaibryk2020@gmail.com>
2025-04-14 16:41:38 +0500

    latest

--- resources/views/stripe-integration/index.blade.php
+++ resources/views/stripe-integration/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <h2>Choose Stripe Payment Option</h2>
-
 <a href="{{ route('stripe.card') }}"><button class="pay-now">Card Payment</button></a>
 <a href="{{ route('stripe.checkout.form') }}"><button class="pay-now">Checkout Payment</button></a>
 <a href="{{ route('stripe.refund.form') }}"><button class="pay-now" style="background-color:red;">Refund</button></a>
